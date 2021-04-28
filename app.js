507a047ff0daebb8a6f0163d62425d8dfdf97532
Brent P. Newhall <brent@brentnewhall.com>
2021-04-28 19:56:00 -0400

    Add EJS

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'ejs');
 
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render( 'sheet' );
 })
 
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
